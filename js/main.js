5f9b1ec9509db92b24c30063dc169c8cd774b9a5
hyein <kimhyein2w@gmail.com>
2026-02-17 17:52:02 +0900

    2026-02-17 hero섹션 첫작업, 헤더 약간 조정

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+/* ===== GSAP 플러그인 ===== */
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(MorphSVGPlugin);
+
+
+
 /* ===== 메인메뉴 ===== */
 const ham = document.querySelector('.ham');
 const mainNav = document.getElementById('mainNav');
@@ -17,6 +23,7 @@ ham.addEventListener('click', () => {
 });
 
 
+
 /* ===== scroll top 버튼 ===== */
 const scrollBtn = document.querySelector('.scroll-top');
 // 버튼 보이는 구간 설정
@@ -28,18 +35,77 @@ window.addEventListener('scroll', () => {
   }
 });
 // 스크롤 실행
-scrollBtn.addEventListener('click', ()=>{
+scrollBtn.addEventListener('click', () => {
   window.scrollTo({
-    top : 0,
+    top: 0,
     behavior: 'smooth',
   });
 });
 
 
 
+/* ===== hero 섹션 ===== */
+/* ----- 타이틀 애니메이션 ----- */
+const titStartShapes = document.querySelectorAll(".title-ani .shape-start");
+const titEndShapes = document.querySelectorAll(".title-ani .shape-end");
+gsap.set(".title-ani__shapes", {
+  opacity: 0,
+  scale: 0.9
+});
 
+/* 주 타임라인 */
+const tlHero = gsap.timeline();
 
+/* 서브 타임라인 : 괄호 움직인 후 모양 등장 */
+const tlHero_show = gsap.timeline({
+  defaults: {
+    ease: "power3.out",
+  }
+});
+tlHero_show
+  .set(".title-ani__brace--left", {
+    x: 0
+  })
+  .set(".title-ani__brace--right", {
+    x: 0
+  })
+  .to(".title-ani__brace--left", {
+    x: -(700 / 2 + 42),
+    duration: 1
+  })
+  .to(".title-ani__brace--right", {
+    x: (700 / 2 + 42),
+    duration: 1
+  }, "<") // 동시에 실행
+  .to(".title-ani__shapes", {
+    opacity: 1,
+    scale: 1,
+    duration: 2,
+    ease: "power2.out",
+  }, "-=0.3"); //직전 애니메이션 끝나는 지점보다 0.3초 앞에서 시작
+tlHero.add(tlHero_show);
 
+/* 서브 타임라인 : 모양을 글자로 바꾸기 */
+const tlHero_shapes = gsap.timeline({
+  defaults: {
+    duration: 2,
+    ease: "expo.inOut",
+  },
+  repeat: -1,
+  yoyo: true,
+  repeatDelay: 1, // yoyo에 지연시간 주기
+});
+
+titStartShapes.forEach((startEl, i) => {
+  tlHero_shapes.to(startEl, {
+    morphSVG: {
+      shape: titEndShapes[i],
+      shapeIndex: "auto"
+    },
+  }, 0); // 전부 동시에
+});
+
+tlHero.add(tlHero_shapes);
 
 
 
@@ -52,7 +118,7 @@ scrollBtn.addEventListener('click', ()=>{
 const breakpoints = {
   md: '(min-width: 800px)',
   lg: '(min-width: 1280px)',
-}
+};
 const mqMiddle = window.matchMedia(breakpoints.md);
 const mqLarge = window.matchMedia(breakpoints.lg);
 
@@ -72,30 +138,26 @@ mqMiddle.addEventListener('change', initResponsive);
 mqLarge.addEventListener('change', initResponsive);
 
 /* --- 반응형 분기별 실행할 함수 --- */
-function mqSmallInit() { // 모바일(디폴트)
+function mqSmallInit() {
+  // 모바일(디폴트)
 
   /* 메인메뉴 */
   mainNav.hidden = true;
   ham.setAttribute('aria-expanded', false);
-
-
 }
 
-function mqMiddleInit() { // min-width: 800px
+function mqMiddleInit() {
+  // min-width: 800px
 
   /* 메인메뉴 */
   mainNav.hidden = true;
   ham.setAttribute('aria-expanded', false);
-
-
 }
 
-function mqLargeInit() { // min-width: 1280px
+function mqLargeInit() {
+  // min-width: 1280px
 
   /* 메인메뉴 */
   mainNav.hidden = false;
   ham.setAttribute('aria-expanded', false);
-
-
-
 }
